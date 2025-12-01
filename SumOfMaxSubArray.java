514e080b859380acc274ca35cb2e8c76f5208976
Pratiksha Nalavade <153375023+PratikshaNalavade@users.noreply.github.com>
2025-12-01 23:10:55 +0530

    Add files via upload

---
+++ SumOfMaxSubArray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class SumOfMaxSubArray{
+    public static void sumOfSubArray(int numbers[]){
+         
+        int max=Integer.MIN_VALUE;
+       
+        for(int i=0; i<numbers.length;i++){
+            int start=i;
+            for(int j=i; j<numbers.length;j++){
+                int end=j;
+                int sum=0;
+                for(int k=start; k<=end;k++){
+                    sum+=numbers[k];
+                }
+               System.out.println("Sum of all subarray:"+sum);
+                if(sum>max){
+                    max=sum;
+                }
+                  
+            }
+           
+        }
+        System.out.println("Maximum sum of subarray:"+max);
+        
+       
+    }
+    public static void main(String args[]){
+        int numbers[]={2,4,6,8,10};
+        sumOfSubArray(numbers);
+    }
+}
