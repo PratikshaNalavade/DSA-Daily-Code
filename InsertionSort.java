766995dcf074c1081107a5cd27204a9cb872efe1
Pratiksha Nalavade <153375023+PratikshaNalavade@users.noreply.github.com>
2025-12-02 22:18:49 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class InsertionSort{
+    public static void insertSort(int arr[]){
+       for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1]=curr;
+            }
+        }
+    
+    public static void print(int arr[]){
+        for(int k=0; k<arr.length;k++){
+            System.out.print(arr[k]+" ");
+        }
+        System.out.println();
+    } 
+    public static void main(String args[]){
+        int arr[]={5,4,2,3,8};
+        insertSort(arr);
+        print(arr);
+    }
+}
