11f5aeea8f5c74668fa3f47f7ddf5af6d925d9cb
Pratiksha Nalavade <153375023+PratikshaNalavade@users.noreply.github.com>
2025-12-13 00:25:23 +0530

    Add files via upload

---
+++ DescendingSelection.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class DescendingSelection{
+    public static void descendingOrder(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]<arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[]={3,5,7,9,2,4};
+        descendingOrder(arr);
+        printArray(arr);
+    }
+}
